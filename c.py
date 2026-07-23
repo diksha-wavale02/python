da3928046dd10f574db3505c72bb93c81faa0c2e
diksha <dikshawavale@gmail.com>
2026-07-23 20:33:30 +0530

    Removed github default files

--- c.py
+++ c.py
@@ -1,23 +1,7 @@
-from p1 import p1
-from p2 import p2
-class c(p1,p2):
-    def pqr(self):
-        print("i'm from child class")
+from A import A
 
-    def call_p2_show(self):
-        return p2.show(self) 
-    
-    def __init__(self,name,age):
-        print("c constructor")
-        p1.__init__(self,name)
-        p2.__init__(self,age)
-
-
-obj=c("ram",20)
-'''
-obj.xyz()
-obj.abc()
-obj.pqr
-obj.show()
-obj.call_p2_show()   
-'''    
+class C(A):
+    def __init__(self,qty):
+        print("from c")
+        super().__init__(3000)
+        self.qty=qty
